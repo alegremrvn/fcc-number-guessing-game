9b38fc4644a44c3774aa66961563e2e28387ac1b
Marvin Alegre <alegremrvn@protonmail.com>
2022-11-09 19:24:59 +0800

    feat: add response to a returning player

--- number_guess.sh
+++ number_guess.sh
@@ -10,3 +10,24 @@ echo "Enter your username:"
 read USERNAME
 
 USERNAME_QUERY_RESULT=$($PSQL "SELECT username FROM users WHERE username='$USERNAME';")
+
+if [[ -z $USERNAME_QUERY_RESULT ]]
+then
+  echo something # added to make the conditional work
+else
+  GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE username='$USERNAME';")
+  BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username='$USERNAME';")
+
+  if [[ -z $BEST_GAME ]]
+  then
+    if [[ $GAMES_PLAYED == 0 ]]
+    then
+      echo "Welcome back $USERNAME. You haven't played any games yet."
+    else
+      echo "Welcome back $USERNAME. You have played $GAMES_PLAYED games."
+    fi
+  else
+    echo "Welcome back $USERNAME. You have played $GAMES_PLAYED games, and your best game \
+    took $BEST_GAME guesses."
+  fi
+fi
